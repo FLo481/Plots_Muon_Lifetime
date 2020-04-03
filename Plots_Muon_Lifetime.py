2cc5c9690d9fd720d8cd31d1ef4f7f1c7296566b
Flo <s347326@uniwuerzburg.onmicrosoft.com>
2020-04-03 15:14:59 -0700

    -added an ODR fit -changed the calculation of the error in the final plot

--- Plots_Muon_Lifetime.py
+++ Plots_Muon_Lifetime.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 import os
 import csv
 import scipy.optimize
+from scipy.odr import ODR, Model, Data, RealData
 
 def fit_func1(x, a, b):
     
     return a*x+b
 
-def fit_func2(x, a, b, c):
+#def fit_func2(x, a, b, c):
 
-    return a*np.exp(-b*x)+c
+#    return a*np.exp(-b*x)+c
+
+def fit_func2(B, x):
+
+    return B[0]*np.exp(-x*B[1])+B[2]
 
 def numberof_datapoints(dirName):
 
@@ -128,7 +133,7 @@ def eval_calibration_file():
     k = 1
     energy, counts = readin_values(dirName)
 
-    #xerr of +/- 1keV
+    #xerr +/- 1 keV
     for l in range(m):
         energy_err_temp.append(1)
     
@@ -137,7 +142,7 @@ def eval_calibration_file():
     del time_temp
     del energy_err_temp
 
-    #plt.errorbar(energy, time, yerr = None, xerr = energy_err , fmt='x', label="Calibration data", markersize=11)
+    #plt.errorbar(energy, time, yerr = None, fmt='x', label="Calibration data", markersize=11)
     params, params_cov = scipy.optimize.curve_fit(fit_func1, energy, time, sigma = None, absolute_sigma = True)
     #plt.plot(energy, fit_func1(energy, params[0], params[1]), label= r"constant fit $y=ax+b$")
     #plt.grid()
@@ -149,7 +154,7 @@ def eval_calibration_file():
     val_time = 1/params[0]
     val_time_err =  perr[0]/params[0]
 
-    #print("1 µs =", val_time, "+/-", perr[0]/params[0], "keV")
+    print("1 µs =", val_time, "+/-", perr[0]/params[0], "keV")
 
     #plt.show()
     #plt.clf()
@@ -165,34 +170,48 @@ def eval_real_data():
     fit = np.empty(m, float)
     counts_err = np.empty(m, float)
     counts_err_temp = []
+    time_err = np.empty(m, float)
+    time_err_temp = []
 
     for l in range(m):
         counts_err_temp.append(counts[l]/(np.sqrt(counts[l])))
+        time_err_temp.append(np.sqrt((1/energy[l])**2+(val_time_err/val_time)**2)*energy[l]/val_time+0.032)
 
     counts_err[:] = counts_err_temp
+    time_err[:] = time_err_temp
+    del counts_err_temp
+    del time_err_temp
 
-    plt.errorbar(energy/val_time, counts, yerr = counts_err, xerr = np.sqrt((1/energy)**2+(val_time_err/val_time)**2)*energy/val_time, fmt='x', label="Detected Muon Decays", markersize=5, zorder = -1)
-    params, params_cov = scipy.optimize.curve_fit(fit_func2, energy/val_time, counts, sigma = counts_err, absolute_sigma = True)
-    plt.plot(energy/val_time, fit_func2(energy/val_time, params[0], params[1], params[2]), label= r"Exponential fit $y=ae^{-bt}+c$")
+    #plt.errorbar(energy/val_time, counts, yerr = counts_err, xerr = np.sqrt((1/energy)**2+(val_time_err/val_time)**2)*energy/val_time, fmt='x', label="Detected Muon Decays", markersize=5, zorder = -1)
+    plt.errorbar(energy/val_time, counts, yerr = counts_err, xerr = time_err, fmt='x', label="Detected Muon Decays", markersize=5, zorder = -1)
+    #params, params_cov = scipy.optimize.curve_fit(fit_func2, energy/val_time, counts, sigma = counts_err, absolute_sigma = True)
+    data = RealData(energy/val_time, counts, time_err, counts_err)
+    model = Model(fit_func2)
+    myodr = ODR(data, model, beta0=[500, 400, 20])
+    myodr.set_job(fit_type=0)
+    myoutput = myodr.run()
+
+    plt.plot(energy/val_time, fit_func2(myoutput.beta, energy/val_time), label= r"Exponential fit $y=ae^{-bt}+c$")
     plt.grid()
     plt.xlabel("Time [$\mu s$]", fontsize=16)
     plt.ylabel("Counts", fontsize=16)
     #plt.legend()
 
-    perr = np.sqrt(np.diag(params_cov))/np.sqrt(len(energy))
-    print("Lifetime of Muon =", 1/params[1]*1000, "+/-", perr[1]/params[1]*1000, "ns")
+    perr = np.sqrt(np.diag(myoutput.cov_beta))/np.sqrt(len(energy))
+    print("Lifetime of Muon =", 1/myoutput.beta[1]*1000, "+/-", perr[1]/myoutput.beta[1]*1000, "ns")
+    print("rel. error", perr[1])
 
     #calculating Chi^2
     chi_squared_value = 0
-    fit[:] = fit_func2(energy/val_time, params[0], params[1], params[2])
+    fit[:] = fit_func2(myoutput.beta, energy/val_time)
 
     for l in range(m):
         chi_squared_value += (counts[l] - fit[l])**2/(np.sqrt(counts[l]))**2 
 
     print("red Chi^2 =", chi_squared_value/(m-3))
-    print("a =", params[0], "+/-", perr[0])
-    print("b =", params[1]*1000, "+/-", perr[1]*1000, "ms")
-    print("c =", params[2], "+/-", perr[2])
+    print("a =", myoutput.beta[0], "+/-", perr[0])
+    print("b =", myoutput.beta[1]*1000, "+/-", perr[1]*1000,  "10^(-3)1/s")
+    print("c =", myoutput.beta[2], "+/-", perr[2])
 
     plt.show()
     plt.clf()
